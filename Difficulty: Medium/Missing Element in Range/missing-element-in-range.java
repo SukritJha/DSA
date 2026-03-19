9ea378af5fff82810ee9c30a79eee2d3ef48e455
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-03-19 21:23:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing Element in Range/missing-element-in-range.java
+++ Difficulty: Medium/Missing Element in Range/missing-element-in-range.java
@@ -2,18 +2,38 @@ class Solution {
     public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
         // code here
       
-        Set<Integer>set = new HashSet<>();
-       // int [] nums = new int[arr[arr.length-1]];
-        for(int i=low;i<=high;i++){
-            set.add(i);
-        }
+      //  Set<Integer>set = new HashSet<>();
+       
+    //     for(int i=low;i<=high;i++){
+    //         set.add(i);
+    //     }
+    //     for(int element : arr){
+    //         if(element>=low && element<=high){
+    //             set.remove(element);
+    //         }
+    //     }
+    //       ArrayList<Integer>list = new ArrayList<>(set);
+    //       Collections.sort(list);
+    //      return list;
+    // 
+         ArrayList<Integer>list = new ArrayList<>();
+        //  for(int element:arr){
+        //      set.add(element);
+        //  }
+        //  for(int i=low;i<=high;i++){
+        //      if(!set.contains(i))  list.add(i);
+        //  }
+        //  return list;
+        
+        //M3
+        boolean[] a = new boolean[high-low+1];
         for(int element : arr){
-            if(element>=low && element<=high){
-                set.remove(element);
-            }
+            if(element>=low && element<=high) a[element-low] = true;
         }
-          ArrayList<Integer>list = new ArrayList<>(set);
-          Collections.sort(list);
-         return list;
+        for(int i=low;i<=high;i++){
+            if(!a[i-low]) list.add(i);
+        }
+        return list;
     }
+    
 }
