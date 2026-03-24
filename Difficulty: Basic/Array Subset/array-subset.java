ae8ff0b475a5cbfad4512d3d3cb1e5da504ee078
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-03-24 19:27:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.java
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -2,6 +2,7 @@
 class Solution {
     public boolean isSubset(int a[], int b[]) {
         // Your code here
+      //  better method using 1 hashmap
         HashMap<Integer,Integer>map = new HashMap<>();
         for(int element : a){
           map.put(element,map.getOrDefault(element,0)+1);
@@ -16,5 +17,22 @@ class Solution {
             }
         }
         return true;
+        
+        // M-2 ussing two HashMaps
+        // HashMap<Integer,Integer>amap = new HashMap<>();
+        // HashMap<Integer,Integer>bmap = new HashMap<>();
+        // for(int ele:a){
+        //     amap.put(ele,amap.getOrDefault(ele,0)+1);
+        // }
+        // for(int ele:b){
+        //     bmap.put(ele,bmap.getOrDefault(ele,0)+1);
+        // }
+        // for(int ele:bmap.keySet()){
+        //     if(!amap.containsKey(ele)) return false;
+        //     int afreq = amap.get(ele);
+        //     int bfreq = bmap.get(ele);
+        //     if(afreq<bfreq) return false;
+        // }
+        // return true;
     }
 }
