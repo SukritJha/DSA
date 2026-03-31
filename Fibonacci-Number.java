d69c3a93edf9e479d619e70c2c685f7bee609bea
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-03-31 19:33:57 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 24.36%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,5 +1,5 @@
 1class Solution {
-2    static int[] dp;
+2   // static int[] dp;
 3    public int fib(int n) {
 4        // if(n==0) return 0;
 5        // if(n==1) return 1;
@@ -7,14 +7,14 @@
 7        // return fib(n-1) + fib(n-2);
 8
 9        // better solution using dp
-10       dp = new int[n+1]; 
-11       return fibo(n); // helper function
+10       int[] memo = new int[n+1]; 
+11       return helper(n,memo); // helper function
 12    }
-13    public int fibo(int n){
+13    public int helper(int n , int[]memo){
 14        if(n<=1) return n;
-15        if(dp[n]!=0) return dp[n];
-16        int ans = fibo(n-1) + fibo(n-2);
-17        dp[n] = ans;
+15        if(memo[n]!=0) return memo[n];
+16        int ans = helper(n-1 , memo) + helper(n-2 , memo);
+17        memo[n] = ans;
 18        return ans;
 19    }
 20}
