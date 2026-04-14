33e266243b79317bb3751584a9ebb6a9b7f5ad04
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-04-14 18:09:38 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 20.28%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,29 +1,44 @@
 1class Solution {
-2   // static int[] dp;
+2// space optimization
 3    public int fib(int n) {
 4        if(n<=1) return n;
-5        int [] arr = new int[n+1];
-6        arr[0] = 0;
-7        arr[1] = 1;
-8        for(int i =2;i<arr.length;i++){
-9            arr[i] = arr[i-1] + arr[i-2];
-10        }
-11        return arr[arr.length-1];
-12        // if(n==0) return 0;
-13        // if(n==1) return 1;
-14        // if(n<=1) return n;
-15        // return fib(n-1) + fib(n-2);
-16        // better solution using dp
-17    //    int[] memo = new int[n+1]; 
-18    //    return helper(n,memo); // helper function
-19    }
-20    // public int helper(int n , int[]memo){
-21    //     if(n<=1) return n;
-22    //     if(memo[n]!=0) return memo[n];
-23    //     // int ans = helper(n-1 , memo) + helper(n-2 , memo);
-24    //     // memo[n] = ans;
-25    //     // return ans;
-26    //     // you can short it also
-27    //     return memo[n] = helper(n-1,memo) + helper(n-2,memo);
-28    // }
-29}
+5        int [] dp = new int[3];
+6        dp[0] = 0;
+7        dp[1] = 1;
+8        for(int i =2;i<=n;i++){
+9            dp[2] = dp[0] + dp[1];
+10            dp[0] = dp[1]; // next time ke liye
+11            dp[1] = dp[2];
+12        }
+13        return dp[2];
+14    }
+15}
+16// class Solution {
+17//    // static int[] dp;
+18//     public int fib(int n) {
+19//         if(n<=1) return n;
+20//         int [] arr = new int[n+1];
+21//         arr[0] = 0;
+22//         arr[1] = 1;
+23//         for(int i =2;i<=n;i++){
+24//             arr[i] = arr[i-1] + arr[i-2];
+25//         }
+26//         return arr[n];
+27//         // if(n==0) return 0;
+28//         // if(n==1) return 1;
+29//         // if(n<=1) return n;
+30//         // return fib(n-1) + fib(n-2);
+31//         // better solution using dp
+32//     //    int[] memo = new int[n+1]; 
+33//     //    return helper(n,memo); // helper function
+34//     }
+35//     // public int helper(int n , int[]memo){
+36//     //     if(n<=1) return n;
+37//     //     if(memo[n]!=0) return memo[n];
+38//     //     // int ans = helper(n-1 , memo) + helper(n-2 , memo);
+39//     //     // memo[n] = ans;
+40//     //     // return ans;
+41//     //     // you can short it also
+42//     //     return memo[n] = helper(n-1,memo) + helper(n-2,memo);
+43//     // }
+44// }
