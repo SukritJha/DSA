37fee8885ba808d54a0701068b10f1d4649183d7
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-04-14 20:06:46 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 92.22%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -8,49 +8,48 @@
 8            dp[1][j] = 1; 
 9        }
 10        for(int i=1;i<m;i++){// loop m-1 times chalega
-11        // first dp wala kaam hoga
-12          for(int j=1;j<n;j++){
-13            dp[1][j] = dp[1][j-1] + dp[0][j];
-14            dp[0][j] = dp[1][j];
-15           }
-16        } 
-17        return dp[1][n-1];
-18    }
-19}    
-20
-21// class Solution {
-22//     public int uniquePaths(int m, int n) {
-23//         // now by tabulation
-24//         int[][]dp = new int[m][n];
-25//         for(int i=0;i<m;i++){
-26//             for(int j=0;j<n;j++){
-27//                 if(i==0||j==0) dp[i][j] = 1;
-28//                 else dp[i][j] = dp[i][j-1] + dp[i-1][j];
-29//             }
-30//         }
-31//         return dp[m-1][n-1];
+11          for(int j=1;j<n;j++){
+12            dp[1][j] = dp[1][j-1] + dp[0][j]; // first dp wala kaam hoga
+13            dp[0][j] = dp[1][j]; // ab neeche wale row lo upar copy paste krdo
+14           }
+15        } 
+16        return dp[1][n-1];
+17    }
+18}    
+19
+20// class Solution {
+21//     public int uniquePaths(int m, int n) {
+22//         // now by tabulation
+23//         int[][]dp = new int[m][n];
+24//         for(int i=0;i<m;i++){
+25//             for(int j=0;j<n;j++){
+26//                 if(i==0||j==0) dp[i][j] = 1;
+27//                 else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+28//             }
+29//         }
+30//         return dp[m-1][n-1];
+31
 32
-33
-34//     //  int [][]dp = new int[m+1][n+1];
-35//     // Arrays.fill(dp,-1); you cannot use this in 2d arrays
-36//     //   for(int[]arr:dp){
-37//     //     Arrays.fill(arr,-1);
-38//     //   }
-39//      // return paths(m,n,dp);
-40//      //  return helper(0,0,m-1,n-1,dp); 
-41//     }
-42//     // memoization M-1
-43//     // private int paths(int m,int n,int[][]dp){
-44//     //     if(m==1||n==1) return 1;
-45//     //     if(dp[m][n] != -1) return dp[m][n];
-46//     //     return dp[m][n] = paths(m,n-1,dp) + paths(m-1,n,dp);
-47//     // }
-48
-49//     // memoization M-2
-50//     // private int helper(int cr,int cl,int lr, int lc,int[][]dp){ // cr = current row , cl = current column
-51//     //     if(cr>lr||cl>lc) return 0; // lr = last row, lc = last column
-52//     //     if(cr==lr && cl==lc) return 1;
-53//     //       if(dp[cr][cl] != -1) return dp[cr][cl];
-54//     //     return dp[cr][cl] = helper(cr+1, cl,lr,lc,dp) + helper(cr,cl+1,lr,lc,dp);
-55//     // }
-56// }
+33//     //  int [][]dp = new int[m+1][n+1];
+34//     // Arrays.fill(dp,-1); you cannot use this in 2d arrays
+35//     //   for(int[]arr:dp){
+36//     //     Arrays.fill(arr,-1);
+37//     //   }
+38//      // return paths(m,n,dp);
+39//      //  return helper(0,0,m-1,n-1,dp); 
+40//     }
+41//     // memoization M-1
+42//     // private int paths(int m,int n,int[][]dp){
+43//     //     if(m==1||n==1) return 1;
+44//     //     if(dp[m][n] != -1) return dp[m][n];
+45//     //     return dp[m][n] = paths(m,n-1,dp) + paths(m-1,n,dp);
+46//     // }
+47
+48//     // memoization M-2
+49//     // private int helper(int cr,int cl,int lr, int lc,int[][]dp){ // cr = current row , cl = current column
+50//     //     if(cr>lr||cl>lc) return 0; // lr = last row, lc = last column
+51//     //     if(cr==lr && cl==lc) return 1;
+52//     //       if(dp[cr][cl] != -1) return dp[cr][cl];
+53//     //     return dp[cr][cl] = helper(cr+1, cl,lr,lc,dp) + helper(cr,cl+1,lr,lc,dp);
+54//     // }
+55// }
