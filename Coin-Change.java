a68abd7cbea17d5297dd3580424b5879a042bc14
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-04-19 09:03:18 +0530

    [322] [Time Beats: 8.46%] [Memory Beats: 5.28%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -5,7 +5,7 @@
 5        for(long[]arr:dp) Arrays.fill(arr,-1);
 6        int ans = (int)helper(0,coins,amount,dp);
 7        if(ans==Integer.MAX_VALUE) return -1; // iska mtlab coins ka ans nhi nikla hmara
-8        return ans;
+8        else return ans;
 9    }
 10    private long helper(int i , int[]coins,int amount,long[][]dp){
 11        if(i==coins.length){
@@ -16,7 +16,7 @@
 16        long skip = helper(i+1,coins,amount,dp); // long isiliye bnaya kyuki 1 + Integer.MAX_VALUE error deta hai as int ki limit khatam ho jati hai
 17        if(coins[i]>amount) return dp[i][amount] = skip;
 18        else{
-19            long pick =1 + helper(i,coins,amount-coins[i],dp);
+19            long pick =1 + helper(i,coins,amount-coins[i],dp); // vvi step
 20            return dp[i][amount] = Math.min(skip,pick);
 21        }
 22    }
