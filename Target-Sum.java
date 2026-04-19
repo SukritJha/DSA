3cb48e2f516f637ed5c6b943aa28e94fa50126e9
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-04-19 22:22:11 +0530

    [494] [Time Beats: 60.48%] [Memory Beats: 10.66%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -11,28 +11,27 @@
 11//         int add =      ways(i+1,nums,target-nums[i]);
 12//         return  add + subtract;
 13//     }
-14// }
+14// }// recursive way not good
 15
-16
-17
-18class Solution {
-19    static int sum;
-20    public int findTargetSumWays(int[] nums, int target) {
-21        int n = nums.length;
-22        sum =0;
-23        for(int ele:nums) sum+=ele;
-24        int[][]dp = new int[n][(2*sum)+1];
-25        for(int[]arr:dp) Arrays.fill(arr,-1);
-26        return ways(0,nums,0,target,dp);
-27    } // hmne result variable bnaya aur hmara goal hai ki usse target tk pahuchana is our goal
-28    private int ways(int i,int[]nums,int result, int target,int[][]dp){
-29        if(i==nums.length) {
-30            if(result==target) return 1; // ye number of ways dega
-31            else return 0;
-32        }
-33        if(dp[i][result+sum]!=-1) return dp[i][result+sum];
-34        int subtract = ways(i+1,nums,result-nums[i],target,dp);
-35        int add = ways(i+1,nums,result+nums[i],target,dp);
-36        return dp[i][result+sum] = add + subtract;
-37    }
-38}
+16// bhai iska dp bnane ka tarika is very very important so remember it
+17class Solution {
+18    static int sum;
+19    public int findTargetSumWays(int[] nums, int target) {
+20        int n = nums.length;
+21        sum =0;
+22        for(int ele:nums) sum+=ele;
+23        int[][]dp = new int[n][2*sum+1];
+24        for(int[]arr:dp) Arrays.fill(arr,-1);
+25        return ways(0,nums,0,target,dp);
+26    } // hmne result variable bnaya aur hmara goal hai ki usse target tk pahuchana is our goal
+27    private int ways(int i,int[]nums,int result, int target,int[][]dp){
+28        if(i==nums.length) {
+29            if(result==target) return 1; // ye number of ways dega
+30            else return 0;
+31        }
+32        if(dp[i][result+sum]!=-1) return dp[i][result+sum];
+33        int subtract = ways(i+1,nums,result-nums[i],target,dp);
+34        int add = ways(i+1,nums,result+nums[i],target,dp);
+35        return dp[i][result+sum] = add + subtract;
+36    }
+37}
