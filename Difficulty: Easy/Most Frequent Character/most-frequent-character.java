c355a742e898a3eb573c53e4750e1ae66188d8a9
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-06-20 14:25:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Most Frequent Character/most-frequent-character.java
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.java
@@ -1,46 +1,20 @@
 class Solution {
     public char getMaxOccuringChar(String s) {
         // code here
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-               int freq = map.get(ch);
-               map.put(ch,freq+1); // for overwriting
-            }
-            else{
-                map.put(ch,1); // pehli baar aa rha hai na kyuki
-            }
-        }
-        int maxfreq = 0;
-        char ans = s.charAt(0); 
-        for(char ch : map.keySet()){
-            int freq = map.get(ch);
-            if(freq>maxfreq ||freq == maxfreq && ch<ans ){
-                maxfreq = freq;
-                ans = ch;
-            } 
-        }
-        return ans;
+         int[]arr = new int[26];
+         for(int i=0;i<s.length();i++){
+             char curr = s.charAt(i); // iss curr ki ek ascii value hogi
+             arr[curr-'a']++; // ye step yaad rkhna hr array aur character wale problem mein ata hai
+         }
+         int maxfreq = -1;
+         char ans = s.charAt(0);
+         for(int i=0;i<arr.length;i++){
+             if(arr[i]>maxfreq){
+                 maxfreq = arr[i];
+                 ans = (char)(i +'a');// wapas character bnane ke liye fir se a ka ascii value add kra
+         }
+             }
+              
+         return ans;
     }
-    // HashMap<Character, Integer> map = new HashMap<>();
-
-    // for (char ch : s.toCharArray()) {
-    //     map.put(ch, map.getOrDefault(ch, 0) + 1);
-    // }
-
-    // char ans = s.charAt(0);
-    // int maxFreq = 0;
-
-    // for (char ch : map.keySet()) {
-    //     int freq = map.get(ch);
-
-    //     if (freq > maxFreq || (freq == maxFreq && ch < ans)) {
-    //         maxFreq = freq;
-    //         ans = ch;
-    //     }
-    // }
-
-    // return ans;
-
 }
