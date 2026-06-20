3348ffee57ec0adf0bab8a58eb9a1240e0bb6c48
Sukrit Jha <143738826+SukritJha@users.noreply.github.com>
2026-06-20 23:10:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Most Frequent Character/most-frequent-character.java
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.java
@@ -1,19 +1,44 @@
+// class Solution {
+//     public char getMaxOccuringChar(String s) {
+//         // code here
+//          int[]arr = new int[26];
+//          for(int i=0;i<s.length();i++){
+//              char curr = s.charAt(i); // iss curr ki ek ascii value hogi
+//              arr[curr-'a']++; // ye step yaad rkhna hr array aur character wale problem mein ata hai
+//          }
+//          int maxfreq = -1;
+//          char ans = s.charAt(0);
+//          for(int i=0;i<arr.length;i++){
+//              if(arr[i]>maxfreq){
+//                  maxfreq = arr[i];
+//                  ans = (char)(i +'a');// wapas character bnane ke liye fir se a ka ascii value add kra
+//          }
+//              }
+              
+//          return ans;
+//     }
+// }
+
+// m2 ye lexographically nhi de payega as hashmap ka koi order nhi hota
 class Solution {
     public char getMaxOccuringChar(String s) {
         // code here
-         int[]arr = new int[26];
-         for(int i=0;i<s.length();i++){
-             char curr = s.charAt(i); // iss curr ki ek ascii value hogi
-             arr[curr-'a']++; // ye step yaad rkhna hr array aur character wale problem mein ata hai
+         HashMap<Character,Integer>map = new HashMap<>();
+         for(char ch : s.toCharArray()){
+             map.put(ch,map.getOrDefault(ch,0)+1);
          }
          int maxfreq = -1;
          char ans = s.charAt(0);
-         for(int i=0;i<arr.length;i++){
-             if(arr[i]>maxfreq){
-                 maxfreq = arr[i];
-                 ans = (char)(i +'a');// wapas character bnane ke liye fir se a ka ascii value add kra
-         }
-             }
+         for(Character key : map.keySet()){
+             if(map.get(key)>maxfreq){
+                 maxfreq = map.get(key);
+                 ans = key;// wapas character bnane ke liye fir se a ka ascii value add kra
+            }
+            else if(map.get(key)==maxfreq && key<ans){
+                ans = key;
+            }
+                
+          }
               
          return ans;
     }
